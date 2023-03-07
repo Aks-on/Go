59ac74c60eb70fe11fda9fed9d6b929bc4f5fba4
Aks-on <32609251+Aks-on@users.noreply.github.com>
2023-03-07 13:08:42 +0300

    Add files via upload

---
+++ Fibonachi.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(fib(6))
+}
+
+func fib(n int) int {
+	//	f := 0
+	if n == 1 {
+		return 1
+	} else if n <= 0 {
+		return 0
+	}
+	return fib(n-2) + fib(n-1)
+}
