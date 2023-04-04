4198a9aa085849c0824916ab5af8702f2ada9130
Ksenia Agapova <agap2001@mail.ru>
2023-04-04 17:37:33 +0300

    attempt 1

--- CodeForces/456A-Laptops.go
+++ CodeForces/456A-Laptops.go
@@ -1,6 +1,33 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
 func main() {
 	//https://codeforces.com/problemset/problem/456/A
+	var n int
+	fmt.Scan(&n)
+	prises := make([]int, n)
+	quality := make([]int, n)
+	reader := bufio.NewReader(os.Stdin)
+	for i := 0; i < n; i++ {
+		fmt.Fscan(reader, &prises[i], &quality[i])
+	}
 
+	check := false
+	for i := 0; i < n; i++ {
+		for j := 1; i+j < n; j++ {
+			if (prises[i] < prises[i+j] && quality[i] > quality[i+j]) || (prises[i] > prises[i+j] && quality[i] < quality[i+j]) {
+				check = true
+			}
+		}
+	}
+	if check {
+		fmt.Println("Happy Alex")
+	} else {
+		fmt.Println("Poor Alex")
+	}
 }
