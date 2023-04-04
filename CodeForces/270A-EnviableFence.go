8d5d5bf39843de91bbadd66accac1799c7ae91c7
Ksenia Agapova <agap2001@mail.ru>
2023-04-04 17:07:00 +0300

    attempt 1

--- CodeForces/270A-EnviableFence.go
+++ CodeForces/270A-EnviableFence.go
@@ -1,6 +1,26 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
 func main() {
 	//https://codeforces.com/problemset/problem/270/A
+	var t int
+	fmt.Scan(&t)
+	reader := bufio.NewReader(os.Stdin)
+	angles := make([]int, t)
+	for i := 0; i < t; i++ {
+		fmt.Fscan(reader, &angles[i])
+	}
 
+	for i := 0; i < t; i++ {
+		if (angles[i]/180+2)%1 == 0 {
+			fmt.Println("YES")
+		} else {
+			fmt.Println("NO")
+		}
+	}
 }
