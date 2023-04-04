df958c63c50485f80268454d05d2ff3bf3f43409
Ksenia Agapova <agap2001@mail.ru>
2023-04-04 13:00:10 +0300

    Добавила счёт вводных данных

--- CodeForces/706B-InterestingDrink.go
+++ CodeForces/706B-InterestingDrink.go
@@ -1,7 +1,19 @@
 package main
 
+import "fmt"
+
 func main() {
 	//https://codeforces.com/problemset/problem/706/B
-	var n int
+	var n, q int
+	fmt.Scan(&n)
+	shops := make([]int, n)
+	for i := 0; i < n; i++ {
+		fmt.Scan(&shops[i])
+	}
+	fmt.Scan(&q)
+	money := make([]int, q)
+	for i := 0; i < q; i++ {
+		fmt.Scan(&money[i])
+	}
 
 }
